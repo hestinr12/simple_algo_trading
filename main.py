b76993397e1a7054a567e26a3746297ef60dda1d
ryan hestin <hestinr12@gmail.com>
2015-06-19 12:22:19 -0400

    cleans initial commit errors in super basic main (way too tired -_-)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import sched
 import time
 import datetime
+from ib.opt import ibConnection, Connection, message
 from modules.router import TwsManager
 try:
 	from modules.live import DemoStrategy
@@ -21,22 +22,22 @@ end_of_day = 18 * 60 *60
 # TWS default settings
 tws_port = 7496
 tws_client_id = 1234
-default_order_id = 1  #not entirely safe...
+default_order_id = 1  # not entirely safe...
 
 
 
-def __main__():
+def main():
 	''' Entry into basic market functionality '''
 	
 	# See examples/data_config.json for some example formats 
-	config = json.load(open('data_config.json', 'r'))
+	config = json.load(open('./data_config.json', 'r'))
 	
 	tws = Connection.create(port=tws_port, clientId=tws_client_id)
-	#tws_manager = # Soon...
+	tws_manager = (tws, config, 1) 
+	#demo_strat = DemoStrategy() # Soon...
 
-	demo_strat = DemoStrategy(tws, config, 1)
 
-
+	# This will be for the actual processes during active hours
 	while True:
 		break
 
